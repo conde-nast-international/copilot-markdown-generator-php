8ae18c7cfa317eb8de409d306359926e3bad44e9
Stef Jones <stef.jones@condenastint.com>
2017-12-08 12:09:19 +0000

    add stub for example implementation

---
+++ example/ContentParser.php
@@ -0,0 +1,10 @@
+<?php
+namespace CopilotTags\Example;
+require __DIR__ . '/../vendor/autoload.php';
+
+class ContentParser {
+
+    public function __construct()
+    {
+    }
+}
---
+++ example/example.php
@@ -0,0 +1,5 @@
+<?php
+namespace CopilotTags\Example;
+require __DIR__ . '/../vendor/autoload.php';
+
+$parser = new ContentParser();
