c5c96fca9ab53f432139af811cb39f2674bb7670
Gray <gmacg.uk@gmail.com>
2017-12-13 12:43:14 +0000

    Link Tag

--- src/Link.php
+++ src/Link.php
@@ -1,10 +1,61 @@
 <?php
 namespace CopilotTags;
 
+function getOuterSpace($text = "") {
+    preg_match("/^\s+/", $text, $lwspace);
+    $lwspace = implode("", $lwspace);
+
+    preg_match("/\s+$/", $text, $rwspace);
+    $rwspace = implode("", $rwspace);
+
+    return ["lwspace" => $lwspace, "rwspace" => $rwspace];
+}
+
 /**
  * Link
  * CommonMark spec: http://spec.commonmark.org/0.27/#links
  */
 class Link extends Text
 {
+
+    public function __construct($text = "", $href = "", $attributes = [])
+    {
+        parent::__construct($text);
+        $this->href = $href;
+        $this->attributes = $attributes;
+    }
+
+    public function write()
+    {
+
+        if ($this->text == "") return self::beautify($this->text);
+        if (preg_match("/\n/", $this->text)) {
+            $tags = explode("\n", $this->text);
+            $tags = array_map(function($tag) {
+              $blockRegex = "/\[#\w+:\s*\S+?(?:\s?(?:\w+:\s?\w+)*?)\](?:\((?:\d+(?:%|em|ex|px|pt|pc|cm|mm|in)?)(?:x?(?:\d+(?:%|em|ex|px|pt|pc|cm|mm|in)?)*)\))*(?:\|{3}[\S\s]*?\|{3})*/";
+              if (preg_match($blockRegex, $tag)) return $tag;
+              if (!trim($tag)) return $tag;
+              return (new Link($tag, $this->href, $this->attributes))->write();
+            }, $tags);
+            return implode("\n", $tags);
+        }
+
+        $text = $this->text;
+
+        $space = getOuterSpace($text);
+        $rwspace = $space["rwspace"];
+        $lwspace = $space["lwspace"];
+
+        $text = trim($text);
+        $href = trim($this->href);
+
+        $attrs = "";
+        if ($this->attributes) {
+            foreach($this->attributes as $key => $value) {
+                $attrs = "$attrs $key=\"$value\"";
+            }
+            $attrs = "{:$attrs }";
+        }
+        return "{$lwspace}[$text]($href){$attrs}{$rwspace}";
+    }
 }
--- tests/LinkTest.php
+++ tests/LinkTest.php
@@ -6,10 +6,76 @@ class LinkTest extends CopilotTagTest
 {
     public function expectedWrites()
     {
+        $assetMarkdown = "\n\n[#image: /photos/123ID]|||some caption|||\n";
+
         return [
             [
-                new Link("Hello world!"),
-                "Hello world!"
+                new Link(),
+                ""
+            ],
+            [
+                new Link(""),
+                ""
+            ],
+            [
+                new Link("Hello world!", "http://li.nk"),
+                "[Hello world!](http://li.nk)"
+            ],
+            [
+                new Link("Hello world!", "http://li.nk", Array("target" => "_blank")),
+                "[Hello world!](http://li.nk){: target=\"_blank\" }"
+            ],
+            [
+                new Link("Hello world!", "http://li.nk", Array("target" => "_blank", "title" => "rawr")),
+                "[Hello world!](http://li.nk){: target=\"_blank\" title=\"rawr\" }"
+            ],
+            [
+                new Link("Hello world!", null, Array("target" => "_blank", "title" => "rawr")),
+                "[Hello world!](){: target=\"_blank\" title=\"rawr\" }"
+            ],
+            [
+                new Link("Hello world!\n\n", "http://li.nk"),
+                "[Hello world!](http://li.nk)\n\n"
+            ],
+            [
+                new Link("  Hello world!\n\n", "http://li.nk"),
+                "  [Hello world!](http://li.nk)\n\n"
+            ],
+            [
+                new Link("  Hello world!\n\n", "http://li.nk"),
+                "  [Hello world!](http://li.nk)\n\n"
+            ],
+            [
+                new Link("Hello world!", "http://li.nk     "),
+                "[Hello world!](http://li.nk)"
+            ],
+            [
+                new Link($assetMarkdown, "http://li.nk"),
+                "$assetMarkdown"
+            ],
+            [
+                new Link("Hello\nworld!", "http://li.nk"),
+                "[Hello](http://li.nk)\n[world!](http://li.nk)"
+            ],
+            [
+                new Link("{$assetMarkdown}{$assetMarkdown}", "http://li.nk"),
+                "{$assetMarkdown}{$assetMarkdown}"
+            ],
+            [
+                new Link("{$assetMarkdown}some text yo{$assetMarkdown}", "http://li.nk"),
+                "{$assetMarkdown}[some text yo](http://li.nk){$assetMarkdown}"
+            ],
+            [
+                new Link("some text yo $assetMarkdown", "http://li.nk"),
+                "[some text yo](http://li.nk) $assetMarkdown"
+            ],
+            [
+                new Link("$assetMarkdown some text yo", "http://li.nk"),
+                "{$assetMarkdown} [some text yo](http://li.nk)"
+            ],
+            [
+                new Link("this is $assetMarkdown some text yo", "http://li.nk"),
+                "[this is](http://li.nk) {$assetMarkdown} [some text yo](http://li.nk)"
             ]
         ];
     }
