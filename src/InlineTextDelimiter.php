28835607bcacfb731ef138212079ae5177261a4c
Stef Jones <stef.jones@condenastint.com>
2018-01-02 11:53:11 +0000

    add InlineTextDelimiter

---
+++ src/InlineTextDelimiter.php
@@ -0,0 +1,14 @@
+<?php
+namespace CopilotTags;
+
+/**
+ * Inline text delimiters
+ */
+abstract class InlineTextDelimiter
+{
+    const EMPHASIS = "*";
+    const STRONG = "**";
+    const SUBSCRIPT = "~";
+    const SUPERSCRIPT = "^";
+    const DELETE = "~~";
+}
