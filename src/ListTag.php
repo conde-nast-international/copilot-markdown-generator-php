3857dc096d9fb9413b787c024e20164102babd65
Stef Jones <stef.jones@condenastint.com>
2018-01-04 16:41:28 +0000

    implement list tag

--- src/ListTag.php
+++ src/ListTag.php
@@ -7,19 +7,40 @@ namespace CopilotTags;
  */
 class ListTag implements CopilotTag
 {
+    const LIST_MARKER_BULLET = "*";
+    const LIST_MARKER_ORDERED = ".";
+
     private $items;
     private $ordered;
 
     public function __construct($items, $ordered = FALSE)
     {
         if (!is_array($items)) throw new \InvalidArgumentException("ListTag::__construct first argument \$items must be an array. Given: ".($items ? "$items " : "")."(".gettype($items).").");
+        foreach($items as $i=>$item) {
+            if (!is_string($item)) throw new \InvalidArgumentException("ListTag::__construct first argument \$items must be an array of strings. Given \$items[$i] = ".($item ? "$item " : "")."(".(gettype($item)).").");
+        }
         $this->items = $items;
         $this->ordered = $ordered;
     }
 
     public function write()
     {
-        // TODO
-        return implode("\n", $this->items);
+        $item_indentation = $this->ordered ? "   " : "  ";
+
+        $tag = "";
+        foreach($this->items as $i=>$item) {
+            $item = trim($item);
+            // indent multiline content
+            $item = preg_replace('/\n/', "\n$item_indentation", $item);
+            // prefix with list marker
+            if($this->ordered) {
+                $item = strval($i + 1).self::LIST_MARKER_ORDERED." $item";
+            } else {
+                $item = self::LIST_MARKER_BULLET." $item";
+            }
+            $tag = "$tag$item\n";
+        }
+        $tag = "$tag\n";
+        return $tag;
     }
 }
--- tests/ListTagTest.php
+++ tests/ListTagTest.php
@@ -1,15 +1,52 @@
 <?php
 use CopilotTags\Tests\CopilotTagTest;
 use CopilotTags\ListTag;
+use CopilotTags\ListItem;
 
 class ListTagTest extends CopilotTagTest
 {
     public function expectedWrites()
     {
         return [
-            "expect plain text" => [
+            "expect unordered list with single item" => [
+                new ListTag(["Hello world!"], FALSE),
+                "* Hello world!\n\n"
+            ],
+            "expect ordered list with single item" => [
+                new ListTag(["Hello world!"], TRUE),
+                "1. Hello world!\n\n"
+            ],
+            "expect default unordered list with single item" => [
                 new ListTag(["Hello world!"]),
-                "Hello world!"
+                "* Hello world!\n\n"
+            ],
+            "expect unordered list with multiple items" => [
+                new ListTag(["Hello", "world!"], FALSE),
+                "* Hello\n* world!\n\n"
+            ],
+            "expect ordered list with multiple items" => [
+                new ListTag(["Hello", "world!"], TRUE),
+                "1. Hello\n2. world!\n\n"
+            ],
+            "expect default unordered list with multiple items" => [
+                new ListTag(["Hello", "world!"]),
+                "* Hello\n* world!\n\n"
+            ],
+            "expect unordered list with single multiline item" => [
+                new ListTag(["Hello\nworld!"], FALSE),
+                "* Hello\n  world!\n\n"
+            ],
+            "expect ordered list with single multiline item" => [
+                new ListTag(["Hello\nworld!"], TRUE),
+                "1. Hello\n   world!\n\n"
+            ],
+            "expect unordered list with multiple multiline items" => [
+                new ListTag(["Hello\nworld!", "Hello\nworld!"], FALSE),
+                "* Hello\n  world!\n* Hello\n  world!\n\n"
+            ],
+            "expect ordered list with multiple multiline items" => [
+                new ListTag(["Hello\nworld!", "Hello\nworld!"], TRUE),
+                "1. Hello\n   world!\n2. Hello\n   world!\n\n"
             ]
         ];
     }
@@ -40,6 +77,31 @@ class ListTagTest extends CopilotTagTest
             "expect string argument to throw InvalidArgumentException" => [
                 ListTag::class,
                 ["Hello world!"],
+                InvalidArgumentException::class
+            ],
+            "expect array of null argument to throw InvalidArgumentException" => [
+                ListTag::class,
+                [[NULL, NULL]],
+                InvalidArgumentException::class
+            ],
+            "expect array of boolean false argument to throw InvalidArgumentException" => [
+                ListTag::class,
+                [[FALSE, FALSE]],
+                InvalidArgumentException::class
+            ],
+            "expect array of boolean true argument to throw InvalidArgumentException" => [
+                ListTag::class,
+                [[TRUE, TRUE]],
+                InvalidArgumentException::class
+            ],
+            "expect array of number argument to throw InvalidArgumentException" => [
+                ListTag::class,
+                [[5, 5]],
+                InvalidArgumentException::class
+            ],
+            "expect array of array argument to throw InvalidArgumentException" => [
+                ListTag::class,
+                [[[], []]],
                 InvalidArgumentException::class
             ]
         ];
