0f7b1493902763c9746418943dc342c1bb980e24
srilq <srilq.git@gmail.com>
2018-01-05 09:37:42 +0000

    implement ListTag (#11)

--- src/ListTag.php
+++ src/ListTag.php
@@ -1,5 +1,6 @@
 <?php
 namespace CopilotTags;
+use CopilotTags\Text;
 
 /**
  * List
@@ -7,19 +8,45 @@ namespace CopilotTags;
  */
 class ListTag implements CopilotTag
 {
+    const LIST_MARKER_BULLET = "*";
+    const LIST_MARKER_ORDERED = ".";
+
     private $items;
     private $ordered;
 
     public function __construct($items, $ordered = FALSE)
     {
         if (!is_array($items)) throw new \InvalidArgumentException("ListTag::__construct first argument \$items must be an array. Given: ".($items ? "$items " : "")."(".gettype($items).").");
+        foreach($items as $i=>$item) {
+            if (!is_string($item)) throw new \InvalidArgumentException("ListTag::__construct first argument \$items must be an array of strings. Given \$items[$i] = ".($item ? "$item " : "")."(".(gettype($item)).").");
+        }
         $this->items = $items;
         $this->ordered = $ordered;
     }
 
     public function write()
     {
-        // TODO
-        return implode("\n", $this->items);
+        $item_indentation = $this->ordered ? "   " : "  ";
+
+        $tag = "";
+        foreach($this->items as $i=>$item) {
+            $text = new Text($item);
+            $item = $text->write();
+            $item = trim($item);
+
+            if($item === "") continue;
+
+            // indent multiline content
+            $item = preg_replace('/\n/', "\n$item_indentation", $item);
+            // prefix with list marker
+            if($this->ordered) {
+                $item = strval($i + 1).self::LIST_MARKER_ORDERED." $item";
+            } else {
+                $item = self::LIST_MARKER_BULLET." $item";
+            }
+            $tag = "$tag$item\n";
+        }
+        if($tag !== "") $tag = "$tag\n";
+        return $tag;
     }
 }
