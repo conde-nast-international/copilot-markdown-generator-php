0cbcb94b5ca6fe3cf56189ae7b7754840ede8509
Stef Jones <stef.jones@condenastint.com>
2018-01-05 17:11:28 +0000

    [squash] remove Section::expectedConstructExceptions

--- tests/SectionTest.php
+++ tests/SectionTest.php
@@ -13,35 +13,4 @@ class SectionTest extends CopilotTagTest
             ]
         ];
     }
-
-    public function expectedConstructExceptions()
-    {
-        return [
-            "expect null \$text argument to throw InvalidArgumentException" => [
-                Section::class,
-                [NULL],
-                InvalidArgumentException::class
-            ],
-            "expect boolean false \$text argument to throw InvalidArgumentException" => [
-                Section::class,
-                [FALSE],
-                InvalidArgumentException::class
-            ],
-            "expect boolean true \$text argument to throw InvalidArgumentException" => [
-                Section::class,
-                [TRUE],
-                InvalidArgumentException::class
-            ],
-            "expect number \$text argument to throw InvalidArgumentException" => [
-                Section::class,
-                [5],
-                InvalidArgumentException::class
-            ],
-            "expect array \$text argument to throw InvalidArgumentException" => [
-                Section::class,
-                [[]],
-                InvalidArgumentException::class
-            ]
-        ];
-    }
 }
